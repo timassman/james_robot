f901781f299822a1692b3b7dc9217db8506b6195
Tim Assman <timassman@users.noreply.github.com>
2023-12-21 11:36:31 +0000

    Added bringup package with launch file to start roomba and joy_teleop

---
+++ james_bringup/launch/james.launch.py
@@ -0,0 +1,33 @@
+# https://roboticsbackend.com/ros2-launch-file-example/
+# https://docs.ros.org/en/humble/Tutorials/Intermediate/Launch/Creating-Launch-Files.html
+# https://docs.ros.org/en/humble/Tutorials/Intermediate/Launch/Using-ROS2-Launch-For-Large-Projects.html#writing-launch-files
+# https://www.youtube.com/watch?v=sl0exwcg3o8
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+import os
+from ament_index_python import get_package_share_directory
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    create_bringup = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('create_bringup'),
+                         'launch/create_2.launch')
+        )
+    )
+
+    joy_teleop = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('create_bringup'),
+                         'launch/joy_teleop.launch')
+        )
+    )
+
+    ld.add_action(create_bringup)
+    ld.add_action(joy_teleop)
+
+    return ld
+
