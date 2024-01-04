b098850cce0b93ad09b8eb409356f093e2db6bc0
Tim Assman <timassman@users.noreply.github.com>
2024-01-04 20:59:38 +0100

    Added launchfile for kinect2

--- james_bringup/launch/rtabmap_kinect2.launch.py
+++ james_bringup/launch/rtabmap_kinect2.launch.py
@@ -1,8 +1,8 @@
 # Source: https://github.com/introlab/rtabmap_ros/blob/ros2/rtabmap_examples/launch/kinect_xbox_360.launch.py
 
 # Requirements:
-#   A kinect for xbox 360
-#   Install kinect_ros2 package (use this fork: https://github.com/matlabbe/kinect_ros2)
+#   A kinect2
+#   Install kinect2_ros2 package (use this fork: https://github.com/timassman/kinect2_ros2.git)
 
 # About launch file arguments:
 # https://answers.ros.org/question/382000/ros2-makes-launch-files-crazy-too-soon-to-be-migrating/
@@ -22,9 +22,9 @@ def generate_launch_description():
           'qos':1}]
 
     remappings=[
-          ('rgb/image', '/kinect/rgb/image_raw'),
-          ('rgb/camera_info', '/kinect/rgb/camera_info'),
-          ('depth/image', '/kinect/depth_registered/image_raw')]
+          ('rgb/image', '/kinect2/rgb/image_raw'),
+          ('rgb/camera_info', '/kinect2/rgb/camera_info'),
+          ('depth/image', '/kinect2/depth_registered/image_raw')]
 
     on_robot = LaunchConfiguration('on_robot')
 
@@ -33,20 +33,20 @@ def generate_launch_description():
         DeclareLaunchArgument(
             'on_robot',
             default_value='true',
-            description='On the headless robot, run kinect, rtabmap and no rviz'
+            description='On the headless robot, run kinect2, rtabmap and no rviz'
         ),
 
         # Nodes to launch
         Node(
-            package='kinect_ros2', executable='kinect_ros2_node', output='screen',
+            package='kinect2_ros2', executable='kinect2_ros2_node', output='screen',
             parameters=[{'depth_registration':True}],
-            namespace="kinect",
+            namespace="kinect2",
             condition=IfCondition(on_robot)),
 
         # Optical rotation
         Node(
             package='tf2_ros', executable='static_transform_publisher', output='screen',
-            arguments=["0", "0", "0", "-1.57", "0", "-1.57", "camera_link", "kinect_rgb"],
+            arguments=["0", "0", "0", "-1.57", "0", "-1.57", "camera_link", "kinect2_rgb"],
             condition=IfCondition(on_robot)),
 
         Node(
@@ -69,5 +69,5 @@ def generate_launch_description():
             parameters=parameters,
             remappings=remappings,
             namespace="rtabmap",
-            condition=UnlessCondition(on_robot)),
+            condition=IfCondition(on_robot)),
     ])
